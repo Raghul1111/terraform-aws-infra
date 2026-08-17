05c395dd0c2081a2a199460fa8df1f1c09b751f2
Raghul D <raghulshyam1111@gmail.com>
2026-08-17 18:29:56 +0000

    Added Terraform AWS EC2 infrastructure code

---
+++ outputs.tf
@@ -0,0 +1,18 @@
+
+output "instance_id" {
+  description = "EC2 Instance ID"
+
+  value = aws_instance.web_server.id
+}
+
+output "public_ip" {
+  description = "EC2 Public IP"
+
+  value = aws_instance.web_server.public_ip
+}
+
+output "private_ip" {
+  description = "EC2 Private IP"
+
+  value = aws_instance.web_server.private_ip
+}
