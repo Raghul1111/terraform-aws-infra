e1a546d0491fdd7b0addd92e84f12b8b0bd29b64
Raghul D <raghulshyam1111@gmail.com>
2026-08-18 19:56:46 +0000

    Add Security Group for EC2

--- security-group.tf
+++ security-group.tf
@@ -2,4 +2,19 @@
 resource "aws_security_group" "web_sg" {
     name = "web_sg"
     description = "Allow TLS inbound traffic and all outbound traffic"
+    
+    ingress {
+    description = "Allow SSH"
+    from_port = 22
+    to_port = 22
+    protocol ="tcp"
+    cidr_blocks = ["0.0.0.0/0"]    
+    }
+    egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
 }
+// For simple usderstanding the security group concept
