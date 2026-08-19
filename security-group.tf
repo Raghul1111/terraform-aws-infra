511e98a13655e0d0689dab8dfa28fed7e682f549
Raghul D <raghulshyam1111@gmail.com>
2026-08-19 20:52:25 +0000

    Add Security Group for EC2

--- security-group.tf
+++ security-group.tf
@@ -1,4 +1,6 @@
-
+data "aws_vpc" "default" {
+  default = true
+}
 resource "aws_security_group" "web_sg" {
   name        = "web_sg"
   description = "Allow TLS inbound traffic and all outbound traffic"
