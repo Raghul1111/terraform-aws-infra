aa717a70940c7ab36f640a6c698a904a5a1f6fb4
Raghul D <raghulshyam1111@gmail.com>
2026-08-20 21:43:31 +0000

    Add VPC networking configuration

--- vpc.tf
+++ vpc.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "main" {
   }
 }
 resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id     = aws_vpc.main.id // Create this subnet inside the VPC that we created as aws_vpc.main 
   cidr_block = "10.0.1.0/24"
 
   tags = {
@@ -31,7 +31,32 @@ resource "aws_route_table" "public" {
     Name = "terraform-public-route-table"
   }
 }
-resource "aws_route_table_association" "public" {
+resource "aws_route_table_association" "public" { // Route Table Association with Public Subnet
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
+}
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id // Create this subnet inside the VPC that we created as aws_vpc.main
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "terraform-private-subnet" // Private Subnet
+  }
+}
+resource "aws_eip" "nat" { //creates an Elastic IP.
+  domain = "vpc"           //tells AWS this Elastic IP is for a VPC
+
+  tags = {
+    Name = "terraform-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id       // Use the Elastic IP we created for this NAT Gateway
+  subnet_id     = aws_subnet.public.id // Put the NAT Gateway inside our Public Subnet
+
+  tags = {
+    Name = "terraform-nat-gateway"
+  }
+  depends_on = [aws_internet_gateway.main] // Make sure the Internet Gateway exists before creating the NAT Gateway
 }
